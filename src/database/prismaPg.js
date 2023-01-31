727444ae0ee87d8b3b502762550a19c1ce4c39fc
Gustavo_DeMaria <gustavo@demaria.com.br>
2023-01-31 17:13:06 -0300

    data base

--- src/database/prismaPg.js
+++ src/database/prismaPg.js
@@ -2,24 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 const prismaClient = new PrismaClient({
     errorFormat: "minimal",
-    log: [
-        {
-          emit: 'stdout',
-          level: 'query',
-        },
-        {
-          emit: 'stdout',
-          level: 'error',
-        },
-        {
-          emit: 'stdout',
-          level: 'info',
-        },
-        {
-          emit: 'stdout',
-          level: 'warn',
-        },
-      ],
+    
 });
 
 //  prismaClient.$on("query", async (e) => {
