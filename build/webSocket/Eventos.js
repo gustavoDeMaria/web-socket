7bdc48454103731a7201c75a4e0b26685f251f05
Gustavo_DeMaria <gustavo@demaria.com.br>
2023-01-31 15:11:09 -0300

    .gitignore is now working

--- build/webSocket/Eventos.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Eventos = void 0;
-var Eventos;
-(function (Eventos) {
-    Eventos["handshake_sucess"] = "handshake-sucess";
-})(Eventos = exports.Eventos || (exports.Eventos = {}));
-//# sourceMappingURL=Eventos.js.map
--- build/webSocket/tratarDisconnect.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tratarDisconnect = void 0;
-function tratarDisconnect(server, client) {
-    server.removerClienteConectado(client);
-    console.log("Disconectado: ", client.id);
-}
-exports.tratarDisconnect = tratarDisconnect;
-//# sourceMappingURL=tratarDisconnect.js.map
