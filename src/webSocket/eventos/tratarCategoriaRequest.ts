2c699c3a0a75350e67eeb1093b3c092b170b9f64
Gustavo_DeMaria <gustavo@demaria.com.br>
2023-01-31 21:31:42 -0300

    logs

--- src/webSocket/eventos/tratarCategoriaRequest.ts
+++ src/webSocket/eventos/tratarCategoriaRequest.ts
@@ -9,12 +9,23 @@ const categorias = new CategoriasIntranet();
 
 export function tratarCategoriaRequest(server: SocketServer, client: SocketIO): (...args: any[]) => void {
     return async () => {
+
+        console.log("entrou")
         const user = server.obterClient(client.id)?.UsuarioIntranet;
+        console.log("user", user)
         
+
         if(user){
             const usuario = await UsuarioIntranet.obterPorLogin(user);
+
+            console.log("usuario", usuario)
+
+
             if (usuario){
                 const categoriaArray = await categorias.obterPorDepartamento(usuario.depto_id);
+
+                console.log("categoriaArray", categoriaArray)
+
                 if (categoriaArray){
                     server.enviarMensagemTo(client.id, Eventos.categoria_response, categoriaArray);
                 }
