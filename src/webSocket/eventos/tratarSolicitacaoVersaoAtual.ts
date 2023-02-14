7957bcda45702b014a8568adf18bbe8fe8981e4d
Gustavo_DeMaria <gustavo@demaria.com.br>
2023-02-14 15:25:46 -0300

    controle de erros

--- src/webSocket/eventos/tratarSolicitacaoVersaoAtual.ts
+++ src/webSocket/eventos/tratarSolicitacaoVersaoAtual.ts
@@ -6,8 +6,16 @@ import fs from 'fs';
 
 export function tratarSolicitacaoVersaoAtual(server: SocketServer, client: SocketIO): (...args: any[]) => void {
     return (request) => {
-        const informacoes = fs.readFileSync('../../update/info.json') as unknown as solicitacao_versao_atual_response;
-        informacoes.URLDownload = process.env.URL + '/update/package.zip';
-        server.enviarMensagem(Eventos.solicitacao_versao_atual_response, informacoes);
-    };
+
+        try {
+            console.log("chegou...");
+
+            const informacoes = fs.readFileSync('../../update/info.json') as unknown as solicitacao_versao_atual_response;
+            informacoes.URLDownload = process.env.URL + '/update/package.zip';
+            server.enviarMensagem(Eventos.solicitacao_versao_atual_response, informacoes);
+    
+        } catch (error) {
+            console.log(error);
+        }
+    }
 }
